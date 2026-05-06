a1f7a7a4d184245411af1ffa268823e72c73ef69
sohail649 <sohailasgharwd@gmail.com>
2026-05-06 20:28:18 +0500

    Fix tailwind config and build issue

--- src/Firebase.js
+++ src/Firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 // 🚀 Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+       
 // 🔐 Auth (Google Login)
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
